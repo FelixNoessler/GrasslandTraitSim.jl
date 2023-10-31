7089e9bae19fbcfe1713a70d94cdfb5931c76e34
Felix Nößler <felixnoessler@aol.com>
2023-10-31 09:52:32 +0100

    removed faulty dependency in docs Project.toml

--- docs/clean_local_doc.jl
+++ docs/clean_local_doc.jl
@@ -1,6 +1,7 @@
 import Pkg
 Pkg.rm("GrasslandTraitSim")
 Pkg.rm("GrasslandTraitVis")
+Pkg.rm("GrasslandTraitValid")
 
 img_path = "docs/src/img/"
 [img[1] == '.' ? "" : rm("$(img_path)$img") for img in readdir(img_path)]
