dcfd5d33409921f7b1bf9a1bdf0e4f88013cd69f
Felix Nößler <felixnoessler@aol.com>
2024-08-15 11:55:28 +0200

    growth function: use the same function as in the manuscript

--- src/3_biomass/1_growth/1_growth.jl
+++ src/3_biomass/1_growth/1_growth.jl
@@ -1,9 +1,9 @@
 include("2_community_potential_growth.jl")
-include("3_community_growth_reducers.jl")
-include("4_light_competition.jl")
-include("5_nutrient_competition.jl")
-include("6_water_competition.jl")
-include("7_root_investment.jl")
+include("3_light_competition.jl")
+include("4_nutrient_competition.jl")
+include("5_water_competition.jl")
+include("6_root_investment.jl")
+include("7_community_growth_reducers.jl")
 
 """
 Calculates the growth of the plant species.
@@ -22,21 +22,21 @@ function growth!(; t, container, above_biomass, total_biomass, actual_height, W,
     ########### Potential growth
     potential_growth!(; container, above_biomass, actual_height, PAR = input.PAR_sum[t])
 
+    ########### Species-specific growth adjustment
+    light_competition!(; container, above_biomass, actual_height)
+    nutrient_reduction!(; container, nutrients, total_biomass)
+    water_reduction!(; container, W, PWP, WHC)
+    root_investment!(; container)
+    @. species_specific_red = light_competition * Nutred * Waterred * root_invest
+
     ########### Community growth adjustment by environmental and seasonal factors
     radiation_reduction!(; container, PAR = input.PAR[t])
     temperature_reduction!(; container, T = input.temperature[t])
     seasonal_reduction!(; container, ST = input.temperature_sum[t])
     community_red = com.RAD * com.SEA * com.TEMP
 
-    ########### Species-specific growth adjustment
-    light_competition!(; container, above_biomass, actual_height)
-    nutrient_reduction!(; container, nutrients, total_biomass)
-    water_reduction!(; container, W, PWP, WHC)
-    root_investment!(; container)
-    @. species_specific_red = light_competition * Waterred * Nutred * root_invest
-
     ########### Final growth
-    @. act_growth = com.potgrowth_total * community_red * species_specific_red
+    @. act_growth = com.potgrowth_total * species_specific_red * community_red
 
     return nothing
 end
