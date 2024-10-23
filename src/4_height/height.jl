03e91c21a6cb2fa3b5e70378945b5ec7c8631ca7
Felix Nößler <felixnoessler@aol.com>
2024-10-23 13:18:46 +0200

    remove fast_height_gain parameter

--- src/4_height/height.jl
+++ src/4_height/height.jl
@@ -6,7 +6,6 @@ function height_dynamic!(; container, actual_height, above_biomass, allocation_a
     @unpack mown, grazed, act_growth,
         height_gain, height_loss_grazing, height_loss_mowing = container.calc
     @unpack nspecies = container.simp
-    @unpack fast_height_gain = container.p
 
     for s in 1:nspecies
         if iszero(above_biomass[s])
@@ -14,7 +13,7 @@ function height_dynamic!(; container, actual_height, above_biomass, allocation_a
             height_loss_mowing[s] = 0u"m"
             height_loss_grazing[s] = 0u"m"
         else
-            height_gain[s] = fast_height_gain * (allocation_above[s] * act_growth[s]) / above_biomass[s] * actual_height[s]
+            height_gain[s] = (allocation_above[s] * act_growth[s]) / above_biomass[s] * actual_height[s]
             height_loss_mowing[s] = mown[s] / above_biomass[s] * actual_height[s]
             height_loss_grazing[s] = grazed[s] / above_biomass[s] * actual_height[s]
         end
