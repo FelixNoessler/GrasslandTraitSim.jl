df2902ce61a1c0b76789ce65dde833da1e45e535
Felix Nößler <felixnoessler@aol.com>
2024-11-05 11:01:43 +0100

    update versions for documentaiton

--- v0.1.1/assets/chunks/selectAll.BgQ1A9PC.js
+++
@@ -1,1 +0,0 @@
-import{S as t,r as n,a as o}from"./transform.Deh2Sj5S.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
