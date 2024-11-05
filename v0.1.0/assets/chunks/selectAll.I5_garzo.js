c6b80f62e883fb6620de3b236dfa942e6fa17423
Felix Nößler <felixnoessler@aol.com>
2024-11-05 12:30:06 +0100

    update build documentation

---
+++ v0.1.0/assets/chunks/selectAll.I5_garzo.js
@@ -0,0 +1,1 @@
+import{S as t,r as n,a as o}from"./transform.DoMEWLv1.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
